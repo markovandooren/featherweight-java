0c4f679b234a386c7df74aac0356d9b0f52071a9
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-10-23 14:48:07 +0200

    bugfixing and further cleanup of the project management code

--- src/chameleon/fj2/input/FJConfigLoader.java
+++ src/chameleon/fj2/input/FJConfigLoader.java
@@ -2,20 +2,21 @@ package chameleon.fj2.input;
 
 import java.io.File;
 
-import chameleon.core.language.Language;
 import chameleon.core.namespace.LazyRootNamespace;
 import chameleon.plugin.LanguagePluginImpl;
 import chameleon.workspace.ConfigElement;
 import chameleon.workspace.ConfigException;
 import chameleon.workspace.ConfigLoader;
 import chameleon.workspace.FileInputSourceFactory;
+import chameleon.workspace.ProjectInitialisationListener;
 import chameleon.workspace.View;
 
 public class FJConfigLoader extends LanguagePluginImpl implements ConfigLoader {
 
 	@Override
-	public ConfigElement createConfigElement(Language language, String projectName, File root) throws ConfigException {
-		View view = new View(new LazyRootNamespace(), language);
+	public ConfigElement createConfigElement(String projectName, File root, ProjectInitialisationListener listener) throws ConfigException {
+		View view = new View(new LazyRootNamespace(), language());
+		if(listener != null) {listener.viewAdded(view);}
 		return new FJProjectConfig(view, new FileInputSourceFactory(), projectName, root);
 	}
 
--- src/chameleon/fj2/input/FJProjectConfig.java
+++ src/chameleon/fj2/input/FJProjectConfig.java
@@ -3,10 +3,10 @@ package chameleon.fj2.input;
 import java.io.File;
 
 import chameleon.workspace.FileInputSourceFactory;
-import chameleon.workspace.PConfig;
+import chameleon.workspace.ProjectConfig;
 import chameleon.workspace.View;
 
-public class FJProjectConfig extends PConfig {
+public class FJProjectConfig extends ProjectConfig {
 
 	public FJProjectConfig(View view, FileInputSourceFactory factory, String projectName, File root) {
 		super(view, factory, projectName, root);
