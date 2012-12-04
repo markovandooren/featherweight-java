0a2a201f3af6790e6fda43adff9e98cc91dcbf9d
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2012-12-04 17:45:28 +0100

    fixed some problems with partial parsing and stale metadata

--- src/chameleon/fj2/input/FJProjectConfig.java
+++ src/chameleon/fj2/input/FJProjectConfig.java
@@ -1,6 +1,8 @@
 package chameleon.fj2.input;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 import chameleon.workspace.FileInputSourceFactory;
 import chameleon.workspace.ProjectConfig;
@@ -10,6 +12,11 @@ public class FJProjectConfig extends ProjectConfig {
 
 	public FJProjectConfig(View view, FileInputSourceFactory factory, String projectName, File root) {
 		super(view, factory, projectName, root);
+	}
+
+	@Override
+	protected List<String> sourceExtensions() {
+		return Collections.singletonList(".fj");
 	}
 
 }
