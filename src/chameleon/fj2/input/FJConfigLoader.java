c8cd24c6e38660d2c078804832841804b9057983
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-01-15 19:28:18 +0100

    update to new chameleon code

--- src/chameleon/fj2/input/FJConfigLoader.java
+++ src/chameleon/fj2/input/FJConfigLoader.java
@@ -7,21 +7,22 @@ import org.rejuse.predicate.SafePredicate;
 
 import chameleon.core.namespace.LazyRootNamespace;
 import chameleon.plugin.LanguagePluginImpl;
+import chameleon.workspace.BootstrapProjectConfig.BaseLibraryConfiguration;
 import chameleon.workspace.ConfigException;
 import chameleon.workspace.ExtensionPredicate;
 import chameleon.workspace.FileInputSourceFactory;
 import chameleon.workspace.ProjectConfigurator;
 import chameleon.workspace.ProjectInitialisationListener;
 import chameleon.workspace.View;
-import chameleon.workspace.BootstrapProjectConfig.BaseLibraryConfiguration;
+import chameleon.workspace.Workspace;
 
 public class FJConfigLoader extends LanguagePluginImpl implements ProjectConfigurator {
 
 	@Override
-	public FJProjectConfig createConfigElement(String projectName, File root, ProjectInitialisationListener listener, BaseLibraryConfiguration baseLibraryConfiguration) throws ConfigException {
+	public FJProjectConfig createConfigElement(String projectName, File root, Workspace workspace, ProjectInitialisationListener listener, BaseLibraryConfiguration baseLibraryConfiguration) throws ConfigException {
 		View view = new View(new LazyRootNamespace(), language());
 		if(listener != null) {listener.viewAdded(view);}
-		return new FJProjectConfig(view, new FileInputSourceFactory(), projectName, root);
+		return new FJProjectConfig(projectName, root, view, workspace, new FileInputSourceFactory());
 	}
 
 	@Override
--- src/chameleon/fj2/input/FJProjectConfig.java
+++ src/chameleon/fj2/input/FJProjectConfig.java
@@ -1,22 +1,16 @@
 package chameleon.fj2.input;
 
 import java.io.File;
-import java.util.Collections;
-import java.util.List;
 
 import chameleon.workspace.FileInputSourceFactory;
 import chameleon.workspace.ProjectConfiguration;
 import chameleon.workspace.View;
+import chameleon.workspace.Workspace;
 
 public class FJProjectConfig extends ProjectConfiguration {
 
-	public FJProjectConfig(View view, FileInputSourceFactory factory, String projectName, File root) {
-		super(projectName, root, view, factory);
-	}
-
-	@Override
-	protected List<String> sourceExtensions() {
-		return Collections.singletonList(".fj");
+	public FJProjectConfig(String projectName, File root, View view, Workspace workspace, FileInputSourceFactory factory) {
+		super(projectName, root, view, workspace, factory);
 	}
 
 }
