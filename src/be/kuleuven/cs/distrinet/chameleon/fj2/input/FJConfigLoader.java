47ea25f85172e340d22ab23bd67bb07b6cafcb2b
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2013-05-29 14:40:36 +0200

    update

--- src/be/kuleuven/cs/distrinet/chameleon/fj2/input/FJConfigLoader.java
+++ src/be/kuleuven/cs/distrinet/chameleon/fj2/input/FJConfigLoader.java
@@ -1,28 +1,16 @@
 package be.kuleuven.cs.distrinet.chameleon.fj2.input;
 
-import java.io.File;
-
-import be.kuleuven.cs.distrinet.chameleon.core.namespace.LazyRootNamespace;
-import be.kuleuven.cs.distrinet.chameleon.plugin.LanguagePluginImpl;
-import be.kuleuven.cs.distrinet.chameleon.workspace.BootstrapProjectConfig.BaseLibraryConfiguration;
 import be.kuleuven.cs.distrinet.chameleon.workspace.ConfigException;
 import be.kuleuven.cs.distrinet.chameleon.workspace.ExtensionPredicate;
 import be.kuleuven.cs.distrinet.chameleon.workspace.FileInputSourceFactory;
+import be.kuleuven.cs.distrinet.chameleon.workspace.ProjectConfiguration;
 import be.kuleuven.cs.distrinet.chameleon.workspace.ProjectConfigurator;
-import be.kuleuven.cs.distrinet.chameleon.workspace.ProjectInitialisationListener;
+import be.kuleuven.cs.distrinet.chameleon.workspace.ProjectConfiguratorImpl;
 import be.kuleuven.cs.distrinet.chameleon.workspace.View;
-import be.kuleuven.cs.distrinet.chameleon.workspace.Workspace;
 import be.kuleuven.cs.distrinet.rejuse.predicate.False;
 import be.kuleuven.cs.distrinet.rejuse.predicate.SafePredicate;
 
-public class FJConfigLoader extends LanguagePluginImpl implements ProjectConfigurator {
-
-	@Override
-	public FJProjectConfig createConfigElement(String projectName, File root, Workspace workspace, ProjectInitialisationListener listener, BaseLibraryConfiguration baseLibraryConfiguration) throws ConfigException {
-		View view = new View(new LazyRootNamespace(), language());
-		if(listener != null) {listener.viewAdded(view);}
-		return new FJProjectConfig(projectName, root, view, workspace, new FileInputSourceFactory());
-	}
+public class FJConfigLoader extends ProjectConfiguratorImpl implements ProjectConfigurator {
 
 	@Override
 	public FJConfigLoader clone() {
@@ -37,6 +25,11 @@ public class FJConfigLoader extends LanguagePluginImpl implements ProjectConfigu
 	@Override
 	public SafePredicate<? super String> binaryFileFilter() {
 		return new False<String>();
+	}
+
+	@Override
+	protected ProjectConfiguration createProjectConfig(View view) throws ConfigException {
+		return new FJProjectConfig(view, new FileInputSourceFactory());
 	}
 
 }
